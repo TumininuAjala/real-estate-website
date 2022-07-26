f09f8e759cb6db31e88449850481b881d06d6c36
Ajala tumininu <ajalatumininu@gmail.com>
2022-07-26 18:01:14 +0100

    tenth commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+   <div>
+  {/* <h1>Hi</h1> */}
+  <Navbar/>
+   </div>
+  );
+}
+
+export default App;
